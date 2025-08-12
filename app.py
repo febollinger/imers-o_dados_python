97193ab89c6cfbed373108ac25b5b942a848f787
unknown <fernandabollinger@icloud.com>
2025-08-11 22:09:49 -0300

    feat: adding graphs

--- app.py
+++ app.py
@@ -71,3 +71,76 @@ col4.metric('Cargo mais frequente', cargo_mais_frequente)
 
 st.markdown('---')
 st.subheader('Gráficos')
+
+col_graph1, col_graph2 = st.columns(2)
+
+#verificação se o dataframe esta vazio, caso não esteja agrupar o cargo com a média salarial dos 10 maiores
+with col_graph1:
+    if not df_filtrado.empty:
+        top_cargos = df_filtrado.groupby('cargo')['usd'].mean().nlargest(10).sort_values(ascending=True).reset_index()
+        grafico_cargos = px.bar(
+            top_cargos,
+            x='usd',
+            y='cargo',
+            orientation='h',
+            title='Top 10 cargos por salário médio',
+            labels={'usd': 'Média salarial anual (USD)', 'cargo': ''}
+        )
+        grafico_cargos.update_layout(title_x=0.1, yaxis={'categoryorder':'total ascending'})
+        st.plotly_chart(grafico_cargos, use_container_width=True)
+    else:
+        st.warning('Nenhum dado para exibir no gráfico de cargos.')
+
+with col_graph2:
+    if not df_filtrado.empty:
+        graph_histogram = px.histogram(
+            df_filtrado,
+            x='usd',
+            nbins=20,
+            title='Distribuição de salários anuais',
+            labels={'usd': 'Faixa salaral - USD', 'count': ''}
+        )
+        graph_histogram.update_layout(title_x = 0.2)
+        st.plotly_chart(graph_histogram, use_container_width= True)
+    else:
+        st.warning('Nenhum dado no gráfico de distribuição')
+
+col_graph3, col_graph4 = st.columns(2)
+
+with col_graph3:
+    if not df_filtrado.empty:
+        tipos_trabalho_contagem = df_filtrado['remoto'].value_counts().reset_index()
+        tipos_trabalho_contagem.columns = ['tipo_trabalho', 'quantidade']
+        graph_remoto = px.pie(
+            tipos_trabalho_contagem,
+            names='tipo_trabalho',
+            values='quantidade',
+            title='Proporção dos tipos de trabalho',
+            hole=0.5
+        )
+
+        graph_remoto.update_traces(textinfo = 'percent+label')
+        graph_remoto.update_layout(title = 'Proporção dos tipos de trabalho')
+        st.plotly_chart(graph_remoto, use_container_width=True)
+    else:
+        st.warning('Nenhum dado para exibir nos tipos de trabalhos')
+
+with col_graph4:
+    if not df_filtrado.empty:
+        cargo_data_scientist = df_filtrado[df_filtrado['cargo'] == 'Data Scientist']
+        #print(df_filtrado.head(10))
+        media_ds = cargo_data_scientist.groupby('residencia_iso3')['usd'].mean().sort_values(ascending=True).reset_index()
+        graph_paises = px.choropleth(
+            media_ds,
+            locations='residencia_iso3',
+            color='usd',
+            color_continuous_scale='rdylgn',
+            title='Salário médio de Cientista de Dados por país',
+            labels={'usd': 'Salário médio (USD)', 'empresa': 'País'})
+        graph_paises.update_layout(title_x=0.1)
+        st.plotly_chart(graph_paises, use_container_width=True)
+    else:
+        st.warning("Nenhum dado para exibir no gráfico de países.")
+
+st.subheader("Dados Detalhados")
+st.dataframe(df_filtrado)
